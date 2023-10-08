03b198ce2817829bbe2efed4554443e1d68ca083
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-10-08 21:55:11 +0900

    import the images for the remaining crew memebers

--- src/crew.js
+++ src/crew.js
@@ -3,8 +3,13 @@ import './assets/css/navbar.css';
 import './assets/css/crew.css';
 
 import commander from './assets/images/crew/image-douglas-hurley.png';
+import specialist from './assets/images/crew/image-mark-shuttleworth.png';
+import pilot from './assets/images/crew/image-victor-glover.png';
+import engineer from './assets/images/crew/image-anousheh-ansari.png';
 
 const img = document.querySelector('[data-crew-img]');
+const nav = document.querySelectorAll('.current-crew li');
+
 
 
 img.src = commander;
