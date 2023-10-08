ea3fac12704d8dee62d951b0ff5d724b1de85d5b
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-10-08 18:09:11 +0900

    import the image for the commander and add it to the page

--- src/crew.js
+++ src/crew.js
@@ -1,3 +1,10 @@
 import './assets/css/typography.css';
 import './assets/css/navbar.css';
 import './assets/css/crew.css';
+
+import commander from './assets/images/crew/image-douglas-hurley.png';
+
+const img = document.querySelector('[data-crew-img]');
+
+
+img.src = commander;
