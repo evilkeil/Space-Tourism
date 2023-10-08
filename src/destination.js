955b655b7824bebbc8922202e97dd2166cd0362a
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-10-08 11:33:55 +0900

    import all the destination pngs

--- src/destination.js
+++ src/destination.js
@@ -3,6 +3,9 @@ import './assets/css/navbar.css';
 import './assets/css/destination.css';
 
 import moon from './assets/images/destination/image-moon.png';
+import mars from './assets/images/destination/image-mars.png';
+import titan from './assets/images/destination/image-titan.png';
+import europa from './assets/images/destination/image-europa.png';
 
 const img = document.querySelector('[data-destination-image]');
 
