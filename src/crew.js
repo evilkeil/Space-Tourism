541a5a7a7f974d4128161327829c7114a2f65ff8
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-10-08 22:23:55 +0900

    create functions to render the descriptions create a function that removes the visible class from all the descriptions create a function that adds a cisible class create a function that removes all the selected class from the nav create a function that adds a selected class add an eventlistner to the ul and apply all the newly created functions

--- src/crew.js
+++ src/crew.js
@@ -9,7 +9,47 @@ import engineer from './assets/images/crew/image-anousheh-ansari.png';
 
 const img = document.querySelector('[data-crew-img]');
 const nav = document.querySelectorAll('.current-crew li');
-
-
+const ul = document.querySelector('.current-crew');
+const descriptions = document.querySelectorAll('.crew-description');
 
 img.src = commander;
+
+ul.addEventListener('click',(e)=>{
+    const target= e.target;
+    if(!(target.tagName.toLowerCase() === 'li')) return;
+    else{
+        const selectedId = target.dataset.selected ;
+        removeVisibleClass(descriptions);
+        addVisibleClass(selectedId,descriptions)
+        removeSelectedClass(nav);
+        addSelectedClass(nav,selectedId);
+        // console.log(selectedId)
+    }
+});
+
+function removeVisibleClass(arr){
+    arr.forEach(element => {
+        element.classList.remove('visible')
+    });
+}
+
+function addVisibleClass(selected,arr){
+    arr.forEach(element=>{
+        if(element.id === selected){
+            element.classList.add('visible');
+        }
+    })
+}
+
+function removeSelectedClass(arr){
+    arr.forEach(element=>{
+        element.classList.remove('selected');
+    })
+}
+function addSelectedClass(arr,selected){
+    arr.forEach(element=>{
+        if(element.dataset.selected === selected){
+            element.classList.add('selected');
+        }
+    })
+}
