95e340463454572fb83f868552e174cc315c9470
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-10-09 12:27:24 +0900

    import images and ready up to style the page

--- src/technology.js
+++ src/technology.js
@@ -1,3 +1,15 @@
 import './assets/css/typography.css';
 import './assets/css/navbar.css';
 import './assets/css/technology.css';
+
+import vehiclePortrait from './assets/images/technology/image-launch-vehicle-portrait.jpg';
+
+
+
+
+
+
+const img = document.querySelector('[data-tech-img]');
+
+
+img.src = vehiclePortrait;
