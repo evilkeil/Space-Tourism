6364e266d07955346a19de6dd163c7918d3c4299
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-10-09 13:35:11 +0900

    set up the event listner

--- src/technology.js
+++ src/technology.js
@@ -5,11 +5,22 @@ import './assets/css/technology.css';
 import vehiclePortrait from './assets/images/technology/image-launch-vehicle-portrait.jpg';
 
 
-
-
-
-
 const img = document.querySelector('[data-tech-img]');
+const nav = document.querySelector('[data-tech-nav]');
+const li = document.querySelectorAll('[data-tech-nav] li');
+const text = document.querySelectorAll('.text');
 
 
 img.src = vehiclePortrait;
+
+
+
+nav.addEventListener('click',(e)=>{
+    const target = e.target;
+    if(!(target.tagName.toLowerCase() === 'li')) return;
+    else{
+        
+    }
+    
+});
+
